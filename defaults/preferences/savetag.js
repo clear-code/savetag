3b7bbb3e08d263c9944a103c8c95cac1123e752d
YUKI Hiroshi <shimoda@clear-code.com>
2018-07-02 17:05:33 +0900

    Fix invalid unicode escape sequence

--- defaults/preferences/savetag.js
+++ defaults/preferences/savetag.js
@@ -1,3 +1,3 @@
-pref("mboximport.export.server_path", "\\\\silver\users\ky1332\mail\msg");
+pref("mboximport.export.server_path", "\\\\silver\\users\\ky1332\\mail\\msg");
 pref("mboximport.export.tagkey", "&label6");
 
